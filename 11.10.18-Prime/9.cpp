66c88e20393882878a78d1a9d17b4b142c7d0247
NesterovN <43090359+NesterovN@users.noreply.github.com>
2018-11-11 23:32:43 +0300

    Update 9.cpp

--- 11.10.18-Prime/9.cpp
+++ 11.10.18-Prime/9.cpp
@@ -1,8 +1,15 @@
+// Нахождение корня функции
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main () {
+	float a;
+	cin >> a;
+	float b;
+	b = sqrt (a);
+	cout << b;
 
 	return 0;
 }
