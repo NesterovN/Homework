7b2b2096070a3e710c7889dc4d36a77a937e0749
NesterovN <43090359+NesterovN@users.noreply.github.com>
2018-11-11 20:20:11 +0300

    Update 1.cpp

--- 11.10.18-Prime/1.cpp
+++ 11.10.18-Prime/1.cpp
@@ -1,18 +1,1 @@
-// Нахождение суммы нечетных цифр целого числа
-#include <iostream>
- 
-using namespace std;
- 
-int main (){
-    int a, b = 0, c ;
-    cin >> a;
-    for (int i=1; a > 0 ; i++) {
-        if (a % 2 == 1) {
-         b += a%10;
-         a = (a - (a%10))/10;
-    }
-    else a = (a - (a%10))/10;
-    }
-    cout << b << endl;
-return 0;
-}
+
