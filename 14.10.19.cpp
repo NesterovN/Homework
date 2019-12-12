70b7461d18434780b4cc3fb4ea37e0036ac47450
NesterovN <43090359+NesterovN@users.noreply.github.com>
2019-12-12 23:36:27 +0300

    Add files via upload

---
+++ 14.10.19.cpp
@@ -0,0 +1,166 @@
+/*Последовательность Дебрёина для k < 10 для ДНК */
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+void Euler(int **ar, vector <int> &d , int a, int b){
+for(int i = 0; i < a; i++){
+   if(ar[i][b] == 1){
+    ar[i][b] = 0;
+    Euler( ar, d, a, i);
+   }
+}
+d.push_back(b);
+}
+int main()
+{
+int n, nn, i, j, k, l, c, d, f, o, s, t, u, v, w, r = 0;
+int ccc, ddd, eee, fff, mmm, ooo, ppp, sss, ttt, uuu = 0;
+int wd, we, wf, wm, wo, wp, ws, wt, wu = 0;
+   cout<<endl;
+    cin>>l;
+    cout<<endl;
+    cout<<endl;
+    cin>>k;
+    cout<<endl;
+    if((l == 0)||(k == 0)){
+        return 0;
+    }
+    l = l - 1;
+    nn = k;
+    for(int i = 1; i < l; i++){
+    nn = nn*k;
+}
+int **a = new int*[nn];
+for(i = 0; i < nn; i++){
+    a[i] = new int[nn];
+    for(j = 0; j < nn; j++){
+    a[i][j] = 0;
+    }
+}
+int *m = new int [l];
+int **b = new int*[l];
+for(int i = 0; i < l; i++){
+    b[i] = new int [nn];
+}
+for(j = 0; j < nn; j++){
+    v = j;
+    for(s = 0; s < l; s++){
+        c = v % k;
+        v = v / k;
+        m[s] = c;
+    }
+    for(i = 0; i < l; i++){
+            o = l - 1 - i;
+    b[o][j] = m[i];
+    }
+}
+for(i = 0; i < nn; i++){
+        cout<<b[0][i];
+        if(l > 1){
+     cout<<b[1][i];
+}
+if(l > 2)cout<<b[2][i];
+if(l > 3)cout<<b[3][i];
+if(l > 4)cout<<b[4][i];
+if(l > 5)cout<<b[5][i];
+if(l > 6)cout<<b[6][i];
+if(l > 7)cout<<b[7][i];
+if(l > 8)cout<<b[8][i];
+if(l > 9)cout<<b[9][i];
+cout<<endl;
+
+l = l + 1;
+nn = nn*k;
+int *p = new int [l];
+int **e = new int*[l];
+for(int i = 0; i < l; i++){
+    e[i] = new int [nn];
+}
+for(j = 0; j < nn; j++){
+    v = j;
+    for(s = 0; s < l; s++){
+        c = v % k;
+        v = v / k;
+        p[s] = c;
+    }
+    for(i = 0; i < l; i++){
+            o = l - 1 - i;
+    e[o][j] = p[i];
+    }
+}
+for(i = 0; i < nn; i++){
+    for(j = 0; j < nn/k; j++){
+        w = 1;
+        d = 1;
+        for(v = 0; v < l - 1; v++){
+            if(b[v][j] != e[v][i]){
+                w = 0;
+            }
+            if(b[v][j] != e[v + 1][i]){
+                d = 0;
+            }
+        }
+            if(w == 1){
+                u = j;
+            }
+            if(d == 1){
+                f = j;
+            }
+    }
+    a[u][f] = 1; r++;
+}
+nn = nn/k;
+int **aa = new int*[nn];
+for(i = 0; i < nn; i++){
+    aa[i] = new int[nn];
+    for(j = 0; j < nn; j++){
+    aa[i][j] = a[i][j];
+    }
+}
+vector <int> rr;
+Euler(aa, rr, nn, 0);
+nn = nn * k;
+cout<<endl;
+for(i = 0; i < nn; i++){
+        cout<<e[0][i];
+        if(l > 1){
+     cout<<e[1][i];
+}
+if(l > 2)cout<<e[2][i];
+if(l > 3)cout<<e[3][i];
+if(l > 4)cout<<e[4][i];
+if(l > 5)cout<<e[5][i];
+if(l > 6)cout<<e[6][i];
+if(l > 7)cout<<e[7][i];
+if(l > 8)cout<<e[8][i];
+if(l > 9)cout<<e[9][i];
+cout<<endl;
+}
+cout<<endl;
+for(i = 0; i < nn/k; i++){
+    for(j = 0; j < nn/k; j++){
+        cout<<" "<<a[i][j]<<" ";
+    }
+    cout<<endl;
+}
+cout<<endl;
+cout<<" DAMN GRAPTHS! ";
+cout<<endl;
+
+cout<<endl;
+l = l - 1;
+for(i = 0; i < r; i++){
+    cout<<rr[i];
+    cout<<endl;
+    v = rr[i];
+    rr[i] = b[l - 1][v];
+}
+cout<<endl;
+cout<<endl;
+for(i = 0; i < r; i++){
+    cout<<rr[i];
+    cout<<endl;
+}
+return 0;
+}
---
+++ 25.09.19.cpp
@@ -0,0 +1,92 @@
+/* ’ранение графов. —писок ребер - reb, матрица смежности - mat, список смежности - sme. «десь перевод из списка ребер в матрицу смежности,
+ из матрицы в списко смежности и из списка смежности в список ребер. „тобы полчуить из матрицы смежности список ребер нужно перевести в
+ список смежности и оттуда в список ребер.
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void rebinmat (int n, int **a, int *b, int *c){
+int l, i, j, m, v;
+while(b[l]){
+    l++;
+}
+for(i = 0; i < l; i++){
+    m = b[i];
+    v = c[i];
+    a[m][v] = 1;
+    a[v][m] = 1;
+}
+}
+
+void matinsme (vector<vector<int> >&vec, int **a, int n){
+     vec.resize(n + 1);
+for( int i = 0; i <= n; i++){
+    for(int j = 0; j <= n; j++){
+        if(a[i][j] == 1){
+            vec[i].push_back(j);
+        }
+    }
+}
+
+  }
+  int& onethree(int** a, int n){
+    int l;
+    for(int i = 0; i <= n; i++){   //7
+    for(int j = 0; j <= n; j++){
+    if(a[i][j] == 1){
+        l++;
+    }
+    }
+}
+l = l/2 + 1;
+int **b = new int*[n + 1];
+for(int i = 0; i <= n; i++){
+    b[i] = new int[l + 1];
+    for(int j = 0; j <= n; j++){
+        b[i][j] = 0;
+    }
+}
+int k = 1;
+for(int i = 0; i <= n; i++){
+    for(int j = i + 1; j <= n; j++){
+        if(a[i][j] == 1){
+            b[i][k] = 1;
+            b[j][k] = 1;
+            k++;
+        }
+    }
+}
+return **b;
+}
+
+void smeinreb (int n, int l, vector<vector<int> > &vec, int *b, int *c){
+    int **d = new int*[n + 1];
+for(int i = 0; i <= n; i++){
+    d[i] = new int[l];
+    for(int j = 0; j <= n; j++){
+        d[i][j] = 0;
+    }
+}
+    for(int i = 0; i <= n; i++){
+    for(int j = 0; j < vec[i].size(); j++){
+           int f = vec[i][j];
+           d[i][f] = 1;
+    }
+}
+int h;
+for(int i = 0; i <= n; i++){
+    for(int j = 0; j <= n; j++){
+        if(d[i][j] == 1){
+            b[h] = i;
+            c[h] = j;
+            h++;
+            d[j][i] = 0;
+        }
+    }
+}
+}
+int main(){
+return 0;
+}
